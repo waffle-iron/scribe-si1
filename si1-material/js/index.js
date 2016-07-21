45aa6f3cc8d1c0333b50712361e69182abab00dd
Gabriel Guimarães <gabrielguimoliver@gmail.com>
2016-07-21 16:08:13 -0300

    correções
    correções

--- si1-material/js/index.js
+++ si1-material/js/index.js
@@ -1,14 +1,3 @@
 $(document).ready( function(){
-  $("#pwd-forgot").click( function() {
-    alert("Esqueceu? Que pena! :(");
-  });
-
-  $("#login-btn").click( function() {
-    alert("Logado");
-  });
-
-  $("#register-btn").click( function() {
-    alert("Registrado");
-  });
   
 });
