c641d86b70e62d5b5049cac28c45f0ca5aa73cbe
Gabriel Guimarães <gabrielguimoliver@gmail.com>
2016-07-22 17:17:32 -0300

    asd
    asd

--- si1-material/js/controllers/controller.js
+++ si1-material/js/controllers/controller.js
@@ -1,3 +1,29 @@
 app.controller('MainCtrl', function($scope){
 
 });
+
+app.controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+
+  $scope.openMenu = buildDelayedToggler('left');
+
+  function debounce(func, wait, context) {
+    var timer;
+    return function debounced() {
+      var context = $scope,
+          args = Array.prototype.slice.call(arguments);
+      $timeout.cancel(timer);
+      timer = $timeout(function() {
+        timer = undefined;
+        func.apply(context, args);
+      }, wait || 10);
+    };
+  }
+
+  function buildDelayedToggler(navID) {
+    return debounce(function() {
+      $mdSidenav(navID)
+        .toggle()
+    }, 200);
+  }
+
+});
