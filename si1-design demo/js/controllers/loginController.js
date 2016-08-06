dfb9552874bc122b0ccc7900b2af2c5f41672fb9
Gabriel Cintra <gabriel.cintra@ccc.ufcg.edu.br>
2016-08-06 17:58:34 -0300

    Design Demo

---
+++ si1-design demo/js/controllers/loginController.js
@@ -0,0 +1,14 @@
+app.controller('MainCtrl', function($scope)
+{
+
+}
+);
+
+app
+.controller('LoginFormCtrl', function($scope)
+{
+	$scope.user = {
+		email: '',
+		password: ''
+	};
+});
---
+++ si1-design demo/js/index.js
@@ -0,0 +1,7 @@
+$(document).ready( function(){
+
+  $("#login-btn").click( function() {
+    window.location.href = "./home.html";
+  })
+
+});
