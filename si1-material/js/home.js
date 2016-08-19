d59fffcff26a7dca21407c5ae94109efc02ce7d2
Marcelo <marceloclp@users.noreply.github.com>
2016-08-19 12:17:43 -0300

    Delete home.js

--- si1-material/js/home.js
+++
@@ -1,8 +0,0 @@
-$.each($('textarea[data-autoresize]'), function() {
-    var offset = this.offsetHeight - this.clientHeight;
-
-    var resizeTextarea = function(el) {
-        $(el).css('height', 'auto').css('height', el.scrollHeight + offset);
-    };
-    $(this).on('keyup input', function() { resizeTextarea(this); }).removeAttr('data-autoresize');
-});
