4e13d1c32dde9c01284e3f50105eea99b30f7768
Gabriel Cintra <gabriel.cintra@ccc.ufcg.edu.br>
2016-08-20 21:01:51 -0300

    Align fix

--- si1-material/app/assets/js/editor.js
+++ si1-material/app/assets/js/editor.js
@@ -1,14 +1,15 @@
 tinymce.init({
   selector: 'div.tinymce',
   theme: 'inlite',
+  language: 'pt_BR',
   entity_encoding : "raw",
   verify_html : false,
   cleanup : false,
-  plugins: 'image table link paste contextmenu textpattern autolink',
-  insert_toolbar: 'quickimage quicktable',
-  selection_toolbar: 'bold italic | quicklink h2 h3 blockquote',
   inline: true,
   paste_data_images: true,
+  plugins: 'image table link paste contextmenu textpattern autolink',
+  insert_toolbar: 'quickimage quicktable',
+  selection_toolbar: 'bold italic styleselect | quicklink blockquote | alignleft aligncenter alignright alignjustify',
   content_css: [
     '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
     '//www.tinymce.com/css/codepen.min.css'    
