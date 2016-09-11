fc9c93e1d2fcf9f0a14ac52627328495c2f0a81c
Gabriel Guimarães <gabriel.oliveira@ccc.ufcg.edu.br>
2016-09-11 17:14:25 -0300

    COMMITAO COM DIALOGS LEGAIS
    Falta as requests pra o backend mas tá bonito que só

--- app/assets/javascripts/controllers/driveController.js
+++ app/assets/javascripts/controllers/driveController.js
@@ -1,14 +1,15 @@
 (function () {
 	angular.module('ScribeApp')
-	.controller('driveController', function ($scope, files) {
+	.controller('driveController', function ($scope, files, $mdDialog, $mdMedia) {
 
 		$scope.pagination = [files.getCurrentFolder()];
 
 		$scope.gridHeader = [
 			{ name: 'Nome', icon: 'sort_by_alpha', col: 5 },
 			{ name: 'Proprietário', icon: 'person', col: 2 },
-			{ name: 'Última Modificação', icon: 'access_time', col: 3 },
-			{ name: 'Tamanho', icon: 'insert_drive_file', col: 2 }
+			{ name: 'Última Modificação', icon: 'access_time', col: 2 },
+			{ name: 'Tamanho', icon: 'insert_drive_file', col: 2 },
+			{ name: 'Ação', icon: 'code', col: 1}
 		];
 
 		$scope.getCurrentFolder = function () {
@@ -41,5 +42,96 @@
 			return index === $scope.pagination.length - 1;
 		};
 
+		$scope.createFolder = function (folderName) {
+			console.log("Criando pasta de nome " + folderName + " dentro da pasta " + $scope.getCurrentFolder().name);
+		};
+
+		$scope.createFile = function (fileName) {
+			console.log("Criando arquivo de nome " + fileName + " dentro da pasta " + $scope.getCurrentFolder().name);
+		};
+
+		$scope.shareFile = function (email) {
+			console.log("Compartilhando com " + email);
+		};
+
+		$scope.createFolderDialog = function(ev) {
+	    var confirm = $mdDialog.prompt()
+	      .title('Criar pasta')
+	      .textContent('A pasta será criada dentro da pasta ' + $scope.getCurrentFolder().name)
+	      .placeholder('Nome da pasta')
+	      .ariaLabel('Nome da pasta')
+	      .targetEvent(ev)
+	      .ok('Criar')
+	      .cancel('Cancelar');
+
+	    $mdDialog.show(confirm).then(function(result) {
+				$scope.createFolder(result);
+			}, function() {
+				console.log("Não criou pasta dentro da pasta " + $scope.getCurrentFolder().name);
+	    });
+	  };
+
+	$scope.createFileDialog = function(ev) {
+		var confirm = $mdDialog.prompt()
+			.title('Criar arquivo')
+			.textContent('Criar um arquivo dentro da pasta ' + $scope.getCurrentFolder().name)
+			.placeholder('Documento sem título')
+			.ariaLabel('Docuemtno sem título')
+			.targetEvent(ev)
+			.ok('Criar')
+			.cancel('Cancelar');
+
+		$mdDialog.show(confirm).then(function(result) {
+			$scope.createFile(result);
+		}, function() {
+			console.log("Não criou arquivo dentro da pasta " + $scope.getCurrentFolder().name);
+		});
+	};
+
+	$scope.shareFileDialog = function(ev) {
+		var confirm = $mdDialog.prompt()
+			.title('Compartilhar arquivo')
+			.textContent('Digite o e-mail da pessoa com quem deseja compartilhar')
+			.placeholder('E-mail')
+			.ariaLabel('E-mail')
+			.targetEvent(ev)
+			.ok('Compartilhar')
+			.cancel('Cancelar');
+
+		$mdDialog.show(confirm).then(function(result) {
+			$scope.shareFile(result);
+		}, function() {
+			console.log("Não compartilhou arquivo");
+		});
+	};
+
+/* CUSTOM DIALOG
+
+	$scope.createFileDialog = function(ev) {
+    $mdDialog.show({
+      controller: DialogController,
+      templateUrl: '../directives/shareDialog.html',
+      parent: angular.element(document.body),
+      targetEvent: ev,
+      clickOutsideToClose:false	,
+    })
+    .then(function(answer) {
+      $scope.createFile(answer);
+    }, function() {
+			console.log("Não criou arquivo dentro da pasta " + $scope.getCurrentFolder().name);
+    });
+  };*/
+
 	});
 })();
+
+function DialogController($scope, $mdDialog) {
+
+  $scope.cancel = function() {
+    $mdDialog.cancel();
+  };
+
+  $scope.answer = function(answer) {
+    $mdDialog.hide(answer);
+  };
+}
--- app/assets/javascripts/services/fileService.js
+++ app/assets/javascripts/services/fileService.js
@@ -6,50 +6,50 @@
 			type: 'folder',
 			update: new Date(),
 			size: '-',
-			owner: 'me',
+			owner: 'eu',
 			contents: [{
 				name: 'Geografia',
 				type: 'folder',
 				update: new Date(),
 				size: '-',
-				owner: 'me',
+				owner: 'eu',
 				contents: [{
 					name: 'Provas',
 					type: 'folder',
 					update: new Date(),
 					size: '-',
-					owner: 'me',
+					owner: 'eu',
 					contents: [{
 						name: 'Primeira Etapa 2015.1',
 						type: 'file',
 						update: new Date(),
 						size: '16 kbs',
-						owner: 'me'
+						owner: 'eu'
 					}, {
 						name: 'Segunda Etapa 2015.1',
 						type: 'file',
 						update: new Date(),
 						size: '12 kbs',
-						owner: 'me'
+						owner: 'eu'
 					}, {
 						name: 'Terceira Etapa 2016.1',
 						type: 'file',
 						update: new Date(),
 						size: '22 kbs',
-						owner: 'me'
+						owner: 'eu'
 					}]
 				}, {
 					name: 'Listas de Exercícios',
 					type: 'folder',
 					update: new Date(),
 					size: '-',
-					owner: 'me',
+					owner: 'eu',
 					contents: [{
 						name: 'Topografia',
 						type: 'file',
 						update: new Date(),
 						size: '8 kbs',
-						owner: 'me'
+						owner: 'eu'
 					}]
 				}]
 			}]
