f2722419b17e6a1f683bec49b972f7f9fce03321
Marcelo <marceloclperrella@gmail.com>
2016-09-12 17:50:36 -0300

    Commented code.

--- app/assets/javascripts/controllers/driveController.js
+++ app/assets/javascripts/controllers/driveController.js
@@ -1,22 +1,10 @@
 (function () {
 	angular.module('ScribeApp')
 	.controller('driveController', function ($scope, $cookies, $timeout, files) {
+		// the user's root folder id, used to make http requests to the server.
 		var current_root_folder_id = $cookies.getObject('current_root_folder_id');
 
-		// current folder
-		/*$scope.currentFolder = files.getRootFolder(current_root_folder_id).then(
-			function (res) { $scope.currentFolder = res.data; },
-			function (err) { console.log(err); }
-		);
-
-		// contents of the current folder
-		$scope.contents = files.getChildren(current_root_folder_id).then(
-			function (res) { $scope.contents = res.data; },
-			function (err) { console.log(err); }
-		);
-
-		$scope.pagination = [$scope.currentFolder];*/
-
+		// options that will be rendered on the table's header.
 		$scope.gridHeader = [
 			{ name: 'Nome', icon: 'sort_by_alpha', col: 5 },
 			{ name: 'Proprietário', icon: 'person', col: 2 },
@@ -24,10 +12,12 @@
 			{ name: 'Tamanho', icon: 'insert_drive_file', col: 2 }
 		];
 
+		// sets the current folder id.
 		$scope.setCurrentFolderId = function (item) {
 			current_root_folder_id = item.id;
 		};
 
+		// sets the current folder.
 		$scope.setCurrentFolder = function (item) {
 			$scope.currentFolder = files.getRootFolder(current_root_folder_id).then(
 				function (res) { $scope.currentFolder = res.data; },
@@ -47,6 +37,7 @@
 				$scope.pagination.push(item);
 		};
 
+		// action() when a file or folder is clicked on the list.
 		$scope.fileAction = function (item) {
 			if (item.type === 'file') {
 				// do something
@@ -67,34 +58,36 @@
 			}
 		};
 
+		// misc function to get the icon based on the file's type.
 		$scope.getIcon = function (item) {
 			if (item.type === 'file') return 'insert_drive_file';
 			else return 'folder';
 		};
 
+		// misc function that determines to show or not the dropdown arrow.
 		$scope.showArrow = function (index) {
 			return index === $scope.pagination.length - 1;
 		};
 
+		// two way data variables.
 		$scope.currentFolder = {};
 		$scope.contents = [];
 		$scope.pagination = [];
 
-		files.getRootFolder(current_root_folder_id) {
+		// initalize the variables above by retrieving the data with http
+		// requests.
+		files.getRootFolder(current_root_folder_id).then(
 			function (res) {
 				$scope.currentFolder = res.data;
 				$scope.pagination.push($scope.currentFolder);
 
-				files.getChildren(current_root_folder_id) {
-					function (res) {
-						$scope.contents = res.data;
-						$scope.$apply();
-					},
+				files.getChildren(current_root_folder_id).then(
+					function (res) { $scope.contents = res.data; },
 					function (err) { console.log(err); }
-				}
+				);
 			},
 			function (err) { console.log(err); }
-		}
+		);
 
 	});
 })();
