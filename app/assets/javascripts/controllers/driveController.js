7d71a506c1afa2441b6be48425f2c525d5247649
Gabriel Cintra <gabriel.cintra@ccc.ufcg.edu.br>
2016-09-14 00:19:54 -0300

    Mudanças na visualização do Drive e textos

--- app/assets/javascripts/controllers/driveController.js
+++ app/assets/javascripts/controllers/driveController.js
@@ -31,9 +31,9 @@
 
 		// options that will be rendered on the table's header.
 		$scope.gridHeaderDrive = [
-			{ name: 'Nome', icon: 'sort_by_alpha', col: 7 },
-			{ name: 'Última Modificação', icon: 'access_time', col: 2 },
-			{ name: 'Ação', icon: 'code', col: 3}
+			{ name: 'Nome', icon: 'sort_by_alpha', col: 5 },
+			{ name: 'Última Modificação', icon: 'access_time', col: 5 },
+			{ name: 'Ação', icon: 'code', col: 2}
 		];
 
 		// sets the current folder id.
@@ -210,8 +210,8 @@
 
 			$scope.createFolderDialog = function(ev) {
 		    var confirm = $mdDialog.prompt()
-		      .title('Criar pasta')
-		      .textContent('A pasta será criada dentro da pasta ' + $scope.currentFolder.name)
+		      .title('Nova pasta')
+		      .textContent('Criar uma pasta em ' + $scope.currentFolder.name)
 		      .placeholder('Nome da pasta')
 		      .ariaLabel('Nome da pasta')
 		      .targetEvent(ev)
@@ -223,7 +223,7 @@
 				}, function() {
 					$mdToast.show(
 						$mdToast.simple()
-							.textContent("Não criou nenhuma pasta dentro de " + $scope.currentFolder.name)
+							.textContent("Não criou nenhuma pasta em " + $scope.currentFolder.name)
 							.position("top right")
 							.hideDelay(3000)
 					);
@@ -232,8 +232,8 @@
 
 		$scope.createFileDialog = function(ev) {
 			var confirm = $mdDialog.prompt()
-				.title('Criar arquivo')
-				.textContent('Criar um arquivo dentro da pasta ' + $scope.currentFolder.name)
+				.title('Novo arquivo')
+				.textContent('Criar um arquivo em' + $scope.currentFolder.name)
 				.placeholder('Documento sem título')
 				.ariaLabel('Documento sem título')
 				.targetEvent(ev)
@@ -245,7 +245,7 @@
 			}, function() {
 				$mdToast.show(
 		      $mdToast.simple()
-		        .textContent('Não criou nenhum arquivo dentro de ' + $scope.currentFolder.name)
+		        .textContent('Não criou nenhum arquivo em ' + $scope.currentFolder.name)
 		        .position("top right")
 		        .hideDelay(3000)
 		    );
@@ -274,9 +274,9 @@
 
 		$scope.renameItemDialog = function(ev, item) {
 			var confirm = $mdDialog.prompt()
-				.title('Renomear item')
-				.textContent('Renomear o item ' + item.name)
-				.placeholder('Nome do item')
+				.title('Renomear')
+				.textContent(item.name + ' terá seu nome alterado.')
+				.placeholder('Insira o novo nome')
 				.ariaLabel('Nome do item')
 				.targetEvent(ev)
 				.ok('Renomear')
@@ -287,7 +287,7 @@
 			}, function() {
 				$mdToast.show(
 		      $mdToast.simple()
-		        .textContent('O arquivo ' + item.name + ' não foi renomeado.')
+		        .textContent(item.name + ' não foi renomeado(a).')
 		        .position("top right")
 		        .hideDelay(3000)
 		    );
